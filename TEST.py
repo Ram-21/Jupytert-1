72757df6f930e2de606db0c0a1aa12d55561a046
Ram-21 <52737428+Ram-21@users.noreply.github.com>
2019-07-13 15:52:16 +0530

    Add files via upload

---
+++ TEST.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+n = int(input("enter a number"))
+c = 0
+for i in range(1,n+1): 
+    if n%1 ==0:
+     c=c+1
+if c==2:
+    print("prime")
+else :
+    print("prime")
+
+
+# In[ ]:
+
+
+
+
